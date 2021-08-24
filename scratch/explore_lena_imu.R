57d560127bf060f8103b1cf1648c55b95575d7e8
John Franchak <franchak@gmail.com>
2021-08-23 21:16:02 -0700

    electronic noise

--- scratch/explore_lena_imu.R
+++ scratch/explore_lena_imu.R
@@ -14,6 +14,7 @@ ggplot(p) +
   ylim(.95,1.1) + theme(legend.position = "top") 
 
 lena <- lena %>% mutate(scaled_ctc = convTurnCount/32/10 + 1,
+                        scaled_tvn = TVN/147.09/10 + 1,
                         clock_time = dateTimeStart_UTC + seconds(150) - hours(7)) %>% 
                 filter(clock_time > start_time, clock_time < stop_time)
 
@@ -21,5 +22,6 @@ lena <- lena %>% mutate(scaled_ctc = convTurnCount/32/10 + 1,
 ggplot(p) + 
   geom_path(aes(x = clock_time, y =.975,color = posture, group = 1L), size = 20) + 
   geom_point(data = lena, aes(x = clock_time, y = scaled_ctc)) + 
+  geom_point(data = lena, aes(x = clock_time, y = scaled_tvn), color = "red") + 
   scale_x_datetime(date_break = "1 hour", date_labels = "%H:%M") + xlab("") + 
   ylim(.95,1.1) + theme(legend.position = "top") 
