52b87067c7b8318531712062f809be7602a08de9
John Franchak <franchak@gmail.com>
2021-12-02 11:25:51 -0800

    Update export_pos.rb

--- step0-datavyu/export_pos.rb
+++ step0-datavyu/export_pos.rb
@@ -13,9 +13,9 @@ begin
 
     for tcell in trial.cells
         out.write((tcell.onset - id.cells[0].onset).to_s + "," + (tcell.offset - id.cells[0].onset).to_s + "," + tcell.pos)
-        out.write("\n")
+        # out.write("\n")
     end
-    out.write((id.cells[1].onset - id.cells[0].onset).to_s + "," + (id.cells[1].offset - id.cells[0].onset).to_s + ",activity\n")
-    out.write((id.cells[2].onset - id.cells[0].onset).to_s + "," + (id.cells[2].offset - id.cells[0].onset).to_s + ",sync\n")
-    out.write((id.cells[3].onset - id.cells[0].onset).to_s + "," + (id.cells[3].offset - id.cells[0].onset).to_s + ",freeplay")
+    # out.write((id.cells[1].onset - id.cells[0].onset).to_s + "," + (id.cells[1].offset - id.cells[0].onset).to_s + ",activity\n")
+    # out.write((id.cells[2].onset - id.cells[0].onset).to_s + "," + (id.cells[2].offset - id.cells[0].onset).to_s + ",sync\n")
+    # out.write((id.cells[3].onset - id.cells[0].onset).to_s + "," + (id.cells[3].offset - id.cells[0].onset).to_s + ",freeplay")
 end
