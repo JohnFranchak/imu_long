65705833f48ad66c075fe2c4aea4873d74957690
John Franchak <franchak@gmail.com>
2022-05-24 16:20:00 -0700

    batching jobs

---
+++ tune_ml/slurm_job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8s
+#SBATCH --mem-per-cpu=1G
+#SBATCH --time=00:15:00 # 60 minutes
+#SBATCH --mail-user=franchak@ucr.edu
+#SBATCH --mail-type=ALL
+#SBATCH --job-name="tidy_models"
+#SBATCH -p intel
+
+Rscript classification_tidymodels.R
