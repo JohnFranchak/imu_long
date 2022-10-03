952c498a45c831f47c4a6fb1fd31a3060ae98931
John Franchak <franchak@gmail.com>
2022-10-03 10:40:28 -0700

    Update compile_lena_imu.R

--- analysis-lena-imu/compile_lena_imu.R
+++ analysis-lena-imu/compile_lena_imu.R
@@ -5,9 +5,10 @@ library(lubridate)
 library(patchwork)
 i_am(".here")
 
-include <- read_csv(here("code","analysis-lena-imu","ppt_include.csv"))
+synced_ppts <- read_csv(here("code","project_status","project_dashboard.csv")) %>% 
+  filter(lena_synced == 1) %>% select(id, session, agemo)
 
-ds <- pmap_dfr(include, ~ read_csv(here("data",..1, ..2,"synced_data","lena_imu_infant.csv")) %>% add_column(age = ..3))
+ds <- pmap_dfr(synced_ppts, ~ read_csv(here("data",..1, ..2,"synced_data","lena_imu_infant.csv")) %>% add_column(age = ..3))
 
 write_csv(ds, here("code","analysis-lena-imu","lena-imu-compiled.csv"))
 
