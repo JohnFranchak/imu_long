663fe68897973398df511ccba91c0257cc4e28ba
John Franchak <franchak@gmail.com>
2022-10-03 10:39:49 -0700

    updated synced lena imu data

--- project_status/sync_lena_bulk.R
+++ project_status/sync_lena_bulk.R
@@ -4,11 +4,8 @@ i_am(".here")
 source(here("code","step3-lena","sync_lena_imu.R"))
 
 synced_ppts <- read_csv(here("code","project_status","project_dashboard.csv")) %>% 
-  filter(lena_processed == 0, lena_downloaded == 1)
+  filter(lena_processed == 2, infant_synced == 1, lena_synced == 0)
 
-walk2(synced_ppts$id, synced_ppts$session, ~ process_its(id = .x, session = .y))
-
-#rm computations from later in the pipeline
-walk(synced_ppts$sessions_dir, ~ file.remove(str_glue("{.x}/synced_data/lena_infant_imu.csv"))) 
+walk2(synced_ppts$id, synced_ppts$session, ~ sync_lena_imu(id = .x, session = .y))
 
 source(here("code","project_status","generate_dashboard.R"))
