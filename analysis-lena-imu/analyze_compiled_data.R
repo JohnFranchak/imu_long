96a0da5336380774a19a1fd34720003bf4615c8a
John Franchak <franchak@gmail.com>
2022-10-17 19:43:33 -0700

    dominance

--- analysis-lena-imu/analyze_compiled_data.R
+++ analysis-lena-imu/analyze_compiled_data.R
@@ -32,7 +32,32 @@ ggplot(ds, aes(x = sit_time, y = adult_word_cnt)) +
 
 ds_long <- ds %>% pivot_longer(cols = sit_time:upright_time, names_to = "position", values_to = "prop") 
 
-ggplot(ds_long, aes(x = clock_time_start_2, y = prop, fill = position)) + 
+ds_long %>% filter(age < 8) %>% 
+ggplot(aes(x = clock_time_start_2, y = prop, fill = position)) + 
   geom_bar(stat = "identity") + 
   facet_wrap("id_uni") + 
   scale_x_time(breaks = breaks_width("2 hour"))
+
+ds_long %>% filter(age >= 8) %>% 
+  ggplot(aes(x = clock_time_start_2, y = prop, fill = position)) + 
+  geom_bar(stat = "identity") + 
+  facet_wrap("id_uni") + 
+  scale_x_time(breaks = breaks_width("2 hour"))
+
+#Dominant body position? 
+
+ds_long %>% 
+  group_by(id_uni, age, clock_time_start_2) %>%
+  filter(nap_time == 0) %>% 
+  summarize(dominant = max(prop, na.rm = T)) %>% 
+  filter(dominant > 0) %>% ungroup %>% 
+  mutate(age_group = factor(ifelse(age < 8, 0, 1), labels = c("Younger","Older"))) %>% 
+  drop_na(dominant, age_group) -> dominance
+
+lmPerm::lmp(dominant ~ age_group, data = dominance) %>% summary
+  
+ggplot(dominance, aes(y = dominant, x = age_group, fill = age_group)) + 
+  geom_boxplot() + geom_jitter()
+
+ggplot(dominance, aes(x = dominant, color = age_group)) + 
+  geom_density()
