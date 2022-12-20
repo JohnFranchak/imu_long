ba0a2c832a5ed8c48b037995c1a156dcb4d1920e
John Franchak <franchak@gmail.com>
2022-12-20 11:32:59 -0800

    Update classification_randforest_featuresets.R

--- tune_ml/classification_randforest_featuresets.R
+++ tune_ml/classification_randforest_featuresets.R
@@ -16,33 +16,39 @@ testing <- slide_filt %>% group_by(code) %>% slice_tail(prop = .25) %>% ungroup 
 not_all_na <- function(x) any(!is.na(x))
 training <- training %>% select_if(not_all_na)
 
+# prediction function
+get_metrics <- function(rfmodel, testing) {
+  predictions <- predict(rfmodel, testing, type = "class")
+  u <- union(predictions, testing$code)
+  res <- confusion_matrix(factor(testing$code, u),factor(predictions, u))
+  print(res$`Balanced Accuracy`)
+  print(res$F1)
+  print(res$`Table`)
+  res
+}
+
 # Baseline = 95% balanced accuracy, 92% F1 score
 rfmodel <- randomForest(code ~ ., data = training, localImp = TRUE, proximity = FALSE, ntree = 150)
-predictions <- predict(rfmodel, testing, type = "class")
-u <- union(predictions, testing$code)
-res <- confusion_matrix(factor(testing$code, u),factor(predictions, u))
-print(res$`Balanced Accuracy`)
-print(res$F1)
-print(res$`Table`)
+get_metrics(rfmodel, testing)
 
-  # Explore parameters
+# Explore parameters
 min_depth_frame <- min_depth_distribution(rfmodel)
-plot_min_depth_distribution(min_depth_frame, k = 30)
-importance_frame <- measure_importance(rfmodel)
+plot_min_depth_distribution(min_depth_frame, k = 50)
 
 #Single sensor/axis features only, 94 balanced and 90.5 F1 score
 training_single <- training %>% select(code:ragyr_MAG)
 rfmodel_single <- randomForest(code ~ ., data = training_single, localImp = TRUE, proximity = FALSE, ntree = 150)
-predictions_single <- predict(rfmodel_single, testing, type = "class")
-u <- union(predictions_single, testing$code)
-res_single <- confusion_matrix(factor(testing$code, u),factor(predictions_single, u))
-print(res_single$`Balanced Accuracy`)
-print(res_single$F1)
-print(res_single$`Table`)
+get_metrics(rfmodel_single, testing)
 
-# Only ankles
+# Only acceleration, 91% balanced an 85% F score
+training_acc <- training %>% select(code, contains("acc"))
+rfmodel_acc <- randomForest(code ~ ., data = training_acc, localImp = TRUE, proximity = FALSE, ntree = 150)
+get_metrics(rfmodel_acc, testing)
 
 
-# Only hips
+# Only gyro is quite bad, 80% balanced and 69% F score
+training_gyr <- training %>% select(code, contains("gyr"))
+rfmodel_gyr <- randomForest(code ~ ., data = training_gyr, localImp = TRUE, proximity = FALSE, ntree = 150)
+get_metrics(rfmodel_gyr, testing)
 
 
