7335d082d1f7a1bb85e8f1fcfc00ff5c5db17613
John Franchak <franchak@gmail.com>
2023-02-08 12:19:36 -0800

    Update compile_agreement.R

--- analysis-part2/compile_agreement.R
+++ analysis-part2/compile_agreement.R
@@ -16,7 +16,7 @@ ds <- ds %>% mutate(file = str_remove(file, "Z:/study_imu_long/data/"),
 
 # OVERALL, UGH
 ds_filt <- ds %>% filter(nap_period == 0 & exclude_period == 0 & !is.na(code) & !is.na(pos))
-ds_filt <- ds %>% filter(exclude_period == 0 & !is.na(code) & !is.na(pos))
+# ds_filt <- ds %>% filter(exclude_period == 0 & !is.na(code) & !is.na(pos))
 
 model <- ds_filt %>% count(file, pos) %>% add_count(file, wt = n, name = "total") %>% mutate(prop_model = n/total)
 human <- ds_filt %>% count(file, code) %>% add_count(file, wt = n, name = "total") %>% mutate(prop_human = n/total)
@@ -34,9 +34,10 @@ ggplot(agree, aes(x = prop_human, y = prop_model)) + facet_wrap("pos") +
   geom_point() + geom_smooth(method = "lm") + xlim(0,1) + ylim(0,1)
 
 # 10 min bins
+ds_filt <- ds %>% filter(nap_period == 0 & exclude_period == 0 & !is.na(code) & !is.na(pos))
 ds_filt <- ds_filt %>% group_by(file) %>% mutate(sample = row_number(), 
                                                  bin = floor(sample/300)) %>% 
-  add_count(file, bin) %>% ungroup() %>% filter(n > 240)
+  add_count(file, bin) %>% ungroup() %>% filter(n > 200)
 
 model <- ds_filt %>% count(file, bin, pos) %>% add_count(file, bin, wt = n, name = "total") %>% mutate(prop_model = n/total)
 human <- ds_filt %>% count(file, bin, code) %>% add_count(file, bin, wt = n, name = "total") %>% mutate(prop_human = n/total)
@@ -59,6 +60,35 @@ ggplot(agree, aes(x = prop_human, y = prop_model, color = file)) + facet_wrap("p
 
 (ind_corr <- agree %>% group_by(file) %>% cor_test(vars = c("prop_model", "prop_human")) %>% arrange(cor))
 # ind_corr %>% write_csv(here("code","analysis-part2", "agree_by_ppt.csv"))
+
+# 5 min bins
+ds_filt <- ds %>% filter(nap_period == 0 & exclude_period == 0 & !is.na(code) & !is.na(pos))
+ds_filt <- ds_filt %>% group_by(file) %>% mutate(sample = row_number(), 
+                                                 bin = floor(sample/150)) %>% 
+  add_count(file, bin) %>% ungroup() %>% filter(n > 130) %>% group_by(file) %>% mutate(num_bins = length(unique(bin))) %>% ungroup()
+
+model <- ds_filt %>% count(file, bin, num_bins, pos) %>% add_count(file, bin,num_bins,  wt = n, name = "total") %>% mutate(prop_model = n/total)
+human <- ds_filt %>% count(file, bin, num_bins,  code) %>% add_count(file, bin, num_bins, wt = n, name = "total") %>% mutate(prop_human = n/total)
+
+agree <- full_join(select(model, file, pos, prop_model, bin, num_bins), 
+                   select(human, file, code, prop_human, bin, num_bins), 
+                   by = c("file" = "file", "pos" = "code", "bin" = "bin", "num_bins" = "num_bins"))
+agree <- agree %>% mutate(prop_human = ifelse(is.na(prop_human), 0, prop_human),
+                          prop_model = ifelse(is.na(prop_model), 0, prop_model))
+agree <- complete(agree, nesting(file, bin, num_bins), pos, fill = list(prop_human = 0, prop_model = 0))
+
+#remove worst performers
+# agree <- agree %>% filter(file != "106/1") %>% filter(file != "107/1")
+
+cor_test(agree, vars = c("prop_model", "prop_human"))
+
+ggplot(agree, aes(x = prop_human, y = prop_model, color = file)) + facet_wrap("pos") + 
+  geom_point() + geom_smooth(method = "lm", se = F) + xlim(0,1) + ylim(0,1) + 
+  theme(legend.position = "bottom")
+
+ggplot(filter(agree, pos == "Sitting", num_bins > 2), aes(x = prop_human, y = prop_model, color = file)) + facet_wrap("file") + 
+  geom_point() + geom_smooth(method = "lm", se = F) + xlim(0,1) + ylim(0,1) + 
+  theme(legend.position = "bottom")
 
 # check individual agree cf pt 1 accuracy - IT IS NOT
 
