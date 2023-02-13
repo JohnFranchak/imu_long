8419378db6c54246d5c6b30cbe7c800429896e84
John Franchak <franchak@gmail.com>
2023-02-13 08:56:47 -0800

    Update compile_lena_imu.R

--- analysis-lena-imu/compile_lena_imu.R
+++ analysis-lena-imu/compile_lena_imu.R
@@ -5,6 +5,8 @@ library(lubridate)
 library(patchwork)
 i_am(".here")
 
+# GROUP MODELS
+
 synced_ppts <- read_csv(here("code","project_status","project_dashboard.csv")) %>% 
   filter(lena_synced == 1) %>% select(id, session, agemo)
 
@@ -12,7 +14,18 @@ ds <- pmap_dfr(synced_ppts, ~ read_csv(here("data",..1, ..2,"synced_data","lena_
 
 write_csv(ds, here("code","analysis-lena-imu","lena-imu-compiled.csv"))
 
+# For BRM paper removing LENA data
 ds %>% select(clockTimeStart:clockTimeEnd, id:age) %>% 
   rename_with(janitor::make_clean_names) %>% 
   write_csv(here("code","analysis-lena-imu","imu-compiled.csv"))
 
+# SPLIT MODELS
+synced_ppts <- synced_ppts %>% filter(!(id == 110 & session == 4), !(id == 111 & session == 4), !(id == 116 & session == 1))
+ds <- pmap_dfr(synced_ppts, ~ read_csv(here("data",..1, ..2,"synced_data","lena_imu_infant_split.csv")) %>% add_column(age = ..3))
+
+write_csv(ds, here("code","analysis-lena-imu","lena-imu-compiled-split.csv"))
+
+# For BRM paper removing LENA data
+ds %>% select(clockTimeStart:clockTimeEnd, id:age) %>% 
+  rename_with(janitor::make_clean_names) %>% 
+  write_csv(here("code","analysis-lena-imu","imu-compiled-split.csv"))
