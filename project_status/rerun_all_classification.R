a69cc87e966415f4f0b9836f6379496cdd498dde
John Franchak <franchak@gmail.com>
2023-03-02 14:39:15 -0800

    updated part 2 with final models

--- project_status/rerun_all_classification.R
+++ project_status/rerun_all_classification.R
@@ -7,8 +7,9 @@ synced_ppts <- read_csv(here("code","project_status","project_dashboard.csv")) %
   filter(infant_synced == 1)
 
 walk2(synced_ppts$id, synced_ppts$session, ~ run_classification_split(id = .x, session = .y, who = "infant", type = "group"))
-walk2(synced_ppts$id, synced_ppts$session, ~ run_classification_split(id = .x, session = .y, who = "infant", type = "sitting"))
+# walk2(synced_ppts$id, synced_ppts$session, ~ run_classification_split(id = .x, session = .y, who = "infant", type = "sitting"))
 
 #Filter out ppts missing categories who can't do split fits
 synced_ppts <- synced_ppts %>% filter(!(id == 110 & session == 4), !(id == 116 & session == 1))
 walk2(synced_ppts$id, synced_ppts$session, ~ run_classification_split(id = .x, session = .y, who = "infant", type = "split"))
+
